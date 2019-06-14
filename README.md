a1183a13b8b6db8323c8c76344e8dfc9fa9096cf
Fload <fload2000@protonmail.com>
2019-06-14 22:01:31 +0200

    Comment changed

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Overview
 This is script for an automated fan control for the Raspberry-Pi. It sets the fan speed depending on the chip temperature. There is even a silentime, where the fan should not spin (f.a at night if the Raspberry-Pi is in your Bedroom and you want to keep it quiet)
+
+# Setup - Hardware
+
+
+# Setup - Script
+
+
+# Start script on startup
+
+
+# License
+
+```
+MIT License
+
+Copyright (c) 2019 Fload
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+```
--- autofan.py
+++ autofan.py
@@ -5,41 +5,57 @@ import sys
 import RPi.GPIO as GPIO
 from time import sleep
 
-# INPUTS
-fanPin = 17 # The Pin ID of the Fan
+# The Pin ID of the Fan
+fanPin = 17 				
 
-desiredTemp = 36 # The maximum temperature in Celsius after which we trigger the fan
-amplitudeTemp = 2 # The amplitdue to the temperature the fan should cool the CPU
+# The maximum temperature in Celsius after which we trigger the fan
+desiredTemp = 36
+# The amplitdue to the temperature the fan should cool the CPU
+amplitudeTemp = 2
 
-delay = 5	# time in secounds
+# Time in secounds
+delay = 5
 
-maxValue = 100	# Max Fan Speed
-minValue = 85 	# Min Fan Speed
+# Max Fan Speed
+maxValue = 100
+# Min Fan Speed
+minValue = 85
+
+# The desired temp the fan cools the chip
+coolingTemp = desiredTemp - amplitudeTemp
+
+# The difference between the two fan speeds
+diffValue = maxValue - minValue
+
+# Variable for the current state of the fan
+fanRunning = False
+
+# Variable for the fan speed the fan will be set to
+fanSpeed = 100
 
 
-coolingTemp = desiredTemp - amplitudeTemp	# The desired temp the fan cools the chip
-
-diffValue = maxValue-minValue	# The defference between the two fan speeds
-
-fanRunning = False	# Variable for knowing the current state of the fan
-
-fanSpeed = 100	# Variable for the fan speed the fan will be set to
-
-starttime = datetime.time(0,0,0)	# Start time when fan should not run
-endtime = datetime.time(8,0,0)		# End time till fan should not run
+# Start time when fan should not run
+starttime = datetime.time(0,0,0)
+# End time till fan should not run
+endtime = datetime.time(8,0,0)
+# Delay between checking if current time is between starttime and endtime
 delaytime = 600
+
 
 # Function for getting the current temperature of the CPU
 def getCPUtemperature():
 	res = os.popen('vcgencmd measure_temp').readline()
 	return float(res.replace("temp=","").replace("'C\n",""))
 
+
 # Function for switching the fan off
 def fanOFF():
 	global fanRunning
 	fanRunning = False
-	myPWM.ChangeDutyCycle(0)   # switch fan off
+	# switch fan off
+	myPWM.ChangeDutyCycle(0)
 	return()
+
 
 # Function for mapping the temperature to the right fan speed
 def mapSpeed(difference):
@@ -51,6 +67,7 @@ def mapSpeed(difference):
 		return value
 	elif value > maxValue:
 		return 100
+
 
 # Function for setting the fan speed automatically
 def autofan():
@@ -75,13 +92,15 @@ def autofan():
 	myPWM.ChangeDutyCycle(fanSpeed)
 	return()
 
+
 # Function for checking time in timerange
 def time_in_range(start, end, x):
     if start <= end:
         return start <= x <= end
     else:
         return start <= x or x <= end	
-	
+
+
 # MAIN
 try:
 	GPIO.setmode(GPIO.BCM)
@@ -93,14 +112,21 @@ try:
 	fanOFF()
 	
 	while True:
-		if time_in_range(starttime, endtime, datetime.datetime.now().time()): # Check if current time is in timerange when fan should not spinning
-			fanOff()			# switch the fan off
-			sleep(delaytime)	# Wait delaytime until to rerun the loop
+		# Check if current time is in timerange when fan should not spinning
+		if time_in_range(starttime, endtime, datetime.datetime.now().time()): 
+			# Switch the fan off
+			fanOff()
+			# Wait delaytime until to rerun the loop
+			sleep(delaytime)
 		else:
-			autofan()				# call def autofan
-			sleep(delay) # Read the temperature every 5 sec, increase or decrease this limit if you want 
-except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt 
-	fanOFF()	# switch the fans off
+			# Call function autofan
+			autofan()
+			# Read the temperature every 5 secounds
+			sleep(delay)
+# trap a CTRL+C keyboard interrupt 
+except KeyboardInterrupt:
+	# Switch the fans off
+	fanOFF()	
 finally:
-	GPIO.cleanup() # resets all GPIO ports used by this program
-
+	# Reset all GPIO ports used by this script
+	GPIO.cleanup()
